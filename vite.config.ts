ef4ae845cf40a12186421211782f07ef42c3efb5
thr3a <thr3a@example.com>
2024-11-02 17:20:49 +0900

    clean: react系削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 /// <reference types="vitest" />
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
   test: {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
   }
